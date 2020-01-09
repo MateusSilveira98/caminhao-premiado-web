194e39f189f6049b5c31de7a7ed39bb4d8f34b68
Mateus da Silveira Costa <cin_msilveira@uolinc.com>
2020-01-09 17:46:53 -0300

    Componente Modal construído

---
+++ src/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="modal" :class="{'is-active': showModal}">
+    <div class="modal-background" @click="closeModal()"></div>
+    <div class="modal-card">
+      <section class="modal-card-body">
+        <slot></slot>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    showModal: { type: Boolean, default: false }
+  },
+  methods: {
+    closeModal() {
+      this.$emit('close', false);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,14 +1,25 @@
 <template>
   <article class="home">
-    <p>HOME</p>
+    <button @click="toggleModal = !toggleModal">Toggle Modal</button>
+    <Modal :showModal="toggleModal" @close="toggleModal = $event">
+      <p>HOME</p>
+    </Modal>
   </article>
 </template>
 
 <script>
 import { mapActions, mapState } from "vuex";
 import _ from "lodash";
+import Modal from '@/components/Modal.vue';
 export default {
- 
+ components: {
+   Modal
+ },
+ data() {
+   return {
+     toggleModal: false
+   }
+ }
 };
 </script>
 
